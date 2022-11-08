65f2f105fbeb762f20f6425802ad24afe7d13b1e
AnanyaGoudar <68283825+AnanyaGoudar@users.noreply.github.com>
2022-11-08 22:31:29 +0530

    initial commit

--- my_travel_journal/src/data.js
+++ my_travel_journal/src/data.js
@@ -4,8 +4,8 @@ export default [
         title: "Namdroling Monastery Bylakuppe",
         location: "Coorg",
         googleMapsUrl: "https://www.google.co.in/maps/place/Namdroling+Monastery+Golden+Temple/@12.4306327,75.9578253,15z/data=!3m1!4b1!4m5!3m4!1s0x3ba5a760db95df17:0xf81bb0392f364ea7!8m2!3d12.4306202!4d75.9665616",
-        startDate: "12 Jan, 2021",
-        endDate: "18 Jan, 2021",
+        startDate: "12 January, 2021",
+        endDate: "18 January, 2021",
         description: "Namdroling Monastery, situated at a distance of 34 km from the Coorg, is the largest teaching centre of the school of Tibetan Buddhism known as Nyingmapa. Popularly known as the 'Golden Temple', the Namdroling Monastery holds some excellent examples of Tibetan architecture and artwork, as seen with the elaborately decorated temple tower and ornate outer walls, intricately adorned with beautiful murals.",
         imageUrl: "./images/Namdroling-Monastery-in-Bylakuppe.jpg"
     },
@@ -64,8 +64,8 @@ export default [
         title: "Jog Falls",
         location: "Shivamogga",
         googleMapsUrl: "https://www.google.co.in/maps/place/Jog+Falls,+Karnataka/@14.2092842,74.643813,11z/data=!3m1!4b1!4m5!3m4!1s0x3bbc0d130bec39cf:0xac178ba925b4429c!8m2!3d14.2003718!4d74.7922202",
-        startDate: "1 jan, 2017",
-        endDate: "6 jan, 2017",
+        startDate: "1 january, 2017",
+        endDate: "6 january, 2017",
         description: "Jog Falls is a waterfall on the Sharavati river located in the Western Ghats running between Uttara Kannada and Shimoga districts of Karnataka, India. It is the third highest plunge waterfall in India.",
         imageUrl: "./images/jog-falls.jpg"
     },
@@ -74,8 +74,8 @@ export default [
         title: "Needle View Hillpoint",
         location: "Ooty",
         googleMapsUrl: "https://www.google.co.in/maps/place/Needle+Rock+View+Point/@11.4992294,76.5120905,15z/data=!3m1!4b1!4m5!3m4!1s0x3ba8a6c6d6b436d3:0x4c4468e4da87df61!8m2!3d11.4992295!4d76.5208453",
-        startDate: "17 Dec, 2016",
-        endDate: "22 Dec, 2016",
+        startDate: "17 December, 2016",
+        endDate: "22 December, 2016",
         description: "Needle View Hillpoint derives its name from a cone-shaped rock-inhabiting it and is much more than just a viewpoint. Leagues apart from a usual sunrise and sunset point, it’s a gorgeous place and is quite different from other sunrise and sunset points.",
         imageUrl: "./images/Needle view hillpoint.jpg"
     },
@@ -84,8 +84,8 @@ export default [
         title: "Mysore Palace",
         location: "Gokarna",
         googleMapsUrl: "https://www.google.co.in/maps/place/Om+Beach/@14.5188271,74.322888,17z/data=!3m1!4b1!4m5!3m4!1s0x3bbe8218126fad05:0x294f4f7ab4235873!8m2!3d14.5192405!4d74.3230039",
-        startDate: "27 Nov, 2015",
-        endDate: "30 Nov, 2015",
+        startDate: "27 November, 2015",
+        endDate: "30 November, 2015",
         description: "Mysore Maharaja Palace is located in the heart of Mysore city. Known for its renowned Mysore Dussehra Festival, it is one of the largest palaces in India.",
         imageUrl: "./images/mysore-palace.webp"
     }
